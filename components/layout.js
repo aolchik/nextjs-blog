a2e893b001fe88b762fda233952887cb48e14357
Alejandro <alejandro@orulo.com.br>
2022-05-19 06:06:07 -0300

    fix: corrects name

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
 
-const name = 'Your Name'
+const name = 'Alejandro'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
